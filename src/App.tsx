7d069b1cf3bffef1f3b762385e5482844bd409c7
Nick <ZippyIO@users.noreply.github.com>
2023-01-31 13:13:42 +1100

    ✨ feat: implement AuthContext to show correct page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-const App = () => (
-    <div className="App">
-        <h1>Hello World!</h1>
-    </div>
-);
+import { signOut } from 'firebase/auth';
+import { useContext, useEffect, useState } from 'react';
+import EntryForm from './components/EntryForm';
+import { AuthContext } from './context/AuthContext';
+import Login from './pages/login/Login';
+import { auth } from './utils/firebase';
+
+const App = () => {
+    const userAuth = useContext(AuthContext);
+    const [user, setUser] = useState<true | false>(false);
+
+    useEffect(() => {
+        setUser(!!userAuth);
+    }, [userAuth]);
+
+    return (
+        <div className="App">
+            {!user && <Login />}
+            {user && (
+                <div>
+                    <EntryForm />
+                    <button
+                        onClick={() => signOut(auth)}
+                        className="p-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 rounded-md"
+                        type="button"
+                    >
+                        Sign out
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default App;
