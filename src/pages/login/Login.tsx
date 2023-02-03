b8de4cc61917314e097b63e8f4a7dd41a933fba3
Nick <ZippyIO@users.noreply.github.com>
2023-02-03 12:39:48 +1100

    💄refactor: login component prettier classNames

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,8 +3,8 @@ import { RiGoogleFill } from 'react-icons/ri';
 import { auth, googleProvider } from '../../utils/firebase';
 
 const Login = () => (
-    <div className="flex justify-center items-center w-screen h-screen">
-        <div className="flex flex-col gap-2 justify-center items-stretch text-center font-opensans bg-zinc-900 p-12 h-42 rounded-xl shadow-xl">
+    <div className="flex h-screen w-screen items-center justify-center">
+        <div className="font-opensans h-42 flex flex-col items-stretch justify-center gap-2 rounded-xl bg-zinc-900 p-12 text-center shadow-xl">
             <div>
                 <h1 className="text-3xl font-extrabold text-blue-500">
                     Neuso - Login
@@ -12,10 +12,10 @@ const Login = () => (
             </div>
             <button
                 onClick={() => signInWithPopup(auth, googleProvider)}
-                className="flex items-center justify-center text-inherit p-1 m-1 h-10 rounded-lg bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700"
+                className="m-1 flex h-10 items-center justify-center rounded-lg bg-emerald-500 p-1 text-inherit transition-colors hover:bg-emerald-600 active:bg-emerald-700"
                 type="button"
             >
-                <div className="flex justify-center items-center">
+                <div className="flex items-center justify-center">
                     <RiGoogleFill size="20px" />
                     Sign in with Google
                     <div className="flex-1" />
