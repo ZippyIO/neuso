597a85ec742d6975636f6cfb33feed466bb632c0
Nick <ZippyIO@users.noreply.github.com>
2023-02-06 13:05:36 +1100

    ✨feat: add redirection to login page if null auth

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,23 @@
+import { useLocation, Navigate } from 'react-router-dom';
 import EntryForm from '../../components/EntryForm';
 import MoodChart from '../../components/MoodChart';
+import Navbar from '../../components/Navbar';
+import { useAuth } from '../../context/AuthContext';
 
-const Dashboard = () => (
-    <div className="flex flex-col">
-        <MoodChart />
-        <EntryForm />
-    </div>
-);
+const Dashboard = () => {
+    const location = useLocation();
+    const { loaded } = useAuth();
 
+    if (!loaded) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return (
+        <div className="flex flex-col">
+            <Navbar />
+            <MoodChart />
+            <EntryForm />
+        </div>
+    );
+};
 export default Dashboard;
