4811e9db639d3cbecbe098b5f827c02443c7f1be
Nick <ZippyIO@users.noreply.github.com>
2023-02-07 12:32:07 +1100

    ♻ refactor: remove unnecessary route duplication

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,23 +1,13 @@
-import { useLocation, Navigate } from 'react-router-dom';
 import EntryForm from '../../components/EntryForm';
 import MoodChart from '../../components/MoodChart';
 import Navbar from '../../components/Navbar';
-import { useAuth } from '../../context/AuthContext';
 
-const Dashboard = () => {
-    const location = useLocation();
-    const { loaded } = useAuth();
+const Dashboard = () => (
+    <div className="flex flex-col">
+        <Navbar />
+        <MoodChart />
+        <EntryForm />
+    </div>
+);
 
-    if (!loaded) {
-        return <Navigate to="/login" state={{ from: location }} replace />;
-    }
-
-    return (
-        <div className="flex flex-col">
-            <Navbar />
-            <MoodChart />
-            <EntryForm />
-        </div>
-    );
-};
 export default Dashboard;
