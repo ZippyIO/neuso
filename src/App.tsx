594751caf190cc4c63eab5243fee2fd8f189d79c
Nick <ZippyIO@users.noreply.github.com>
2023-02-07 12:34:07 +1100

    ✨feat: add home and signup page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import ProtectedRoutes from './components/ProtectedRoutes';
 import Dashboard from './pages/dashboard/Dashboard';
+import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
 
 const App = () => (
     <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
 
         <Route element={<ProtectedRoutes />}>
             <Route path="/dashboard" element={<Dashboard />} />
