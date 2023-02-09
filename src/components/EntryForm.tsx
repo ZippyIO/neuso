66e2561bc2227a1f7878ea842ab24edaa791cd06
Nick <ZippyIO@users.noreply.github.com>
2023-02-09 14:23:21 +1100

    ✨feat: add tag menu, title & description fields
    Updated submitting data to firestore to include new fields.

--- src/components/EntryForm.tsx
+++ src/components/EntryForm.tsx
@@ -12,23 +12,36 @@ import {
     MoodRadioHorrible,
     MoodRadioOkay,
 } from './MoodRadios';
+import Select from './Select';
 
 const EntryForm = () => {
     const { user } = useAuth();
-    const [mood, setMood] = useState<number>();
+    const [tags, setTags] = useState([
+        'home',
+        'work',
+        'busy',
+        'sick',
+        'exercise',
+    ]);
+    const [selectedTags, setSelectedTags] = useState<Array<string>>([]);
     const [date, setDate] = useState<Date>(new Date());
     const [datePicker, setDatePicker] = useState<DateValueType>({
         startDate: new Date(),
         endDate: new Date(),
     });
+    const [mood, setMood] = useState<number>();
 
-    const handleFormSubmit = async (e: React.FormEvent<HTMLElement>) => {
+    const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
+        const { entrytitle, entrydescription } = e.target as HTMLFormElement;
         const userDoc = doc(firestore, 'users', user?.uid as string);
         await addDoc(collection(userDoc, 'mood'), {
             date: Timestamp.fromDate(date),
+            tags: selectedTags,
             mood,
+            title: entrytitle.value,
+            description: entrydescription.value,
         });
     };
 
@@ -41,6 +54,10 @@ const EntryForm = () => {
         setDate(dayjs(newDate?.startDate).toDate());
     };
 
+    const handleSelectTag = (value: string) => {
+        setSelectedTags((prevState) => [...prevState, value]);
+    };
+
     return (
         <form
             action=""
@@ -49,6 +66,9 @@ const EntryForm = () => {
         >
             <div className="flex flex-col gap-2 rounded-lg bg-zinc-800 p-4 text-center">
                 <Datepicker
+                    inputClassName="dark:bg-zinc-900/40"
+                    toggleClassName="dark:bg-emerald-600 rounded-md"
+                    primaryColor="emerald"
                     value={datePicker}
                     useRange={false}
                     asSingle
@@ -61,7 +81,7 @@ const EntryForm = () => {
                     How was your day today?
                 </h3>
                 <div
-                    className="grid grid-cols-5 justify-center gap-2 rounded-xl bg-zinc-800 p-2"
+                    className="grid grid-cols-5 justify-center gap-2 rounded-xl bg-zinc-800 p-2 text-center"
                     onChange={handleMoodChange}
                 >
                     <MoodRadioHorrible />
@@ -69,6 +89,44 @@ const EntryForm = () => {
                     <MoodRadioOkay />
                     <MoodRadioGood />
                     <MoodRadioExcellent />
+                </div>
+                <label htmlFor="entrytitle" className="flex flex-col">
+                    <span className=" font-semibold text-zinc-200">Title</span>
+                    <input
+                        type="text"
+                        name="entrytitle"
+                        id="entrytitle"
+                        className="rounded-md bg-zinc-900/40 p-1.5"
+                    />
+                </label>
+                <label htmlFor="entrydescription" className="flex flex-col">
+                    <span className="font-semibold text-zinc-200">
+                        Description
+                    </span>
+                    <textarea
+                        name="entrydescription"
+                        id="entrydescription"
+                        className="rounded-md bg-zinc-900/40 p-1"
+                    />
+                </label>
+                <div className="flex flex-col gap-2">
+                    <div className="flex justify-center gap-2">
+                        {selectedTags.map((tag) => (
+                            <div
+                                key={tag}
+                                className="w-fit rounded-md bg-blue-500 px-1.5 py-0 text-sm font-medium text-zinc-100"
+                            >
+                                {tag}
+                            </div>
+                        ))}
+                    </div>
+                    <div>
+                        <Select
+                            values={tags}
+                            selectedValues={selectedTags}
+                            handleValueChange={handleSelectTag}
+                        />
+                    </div>
                 </div>
                 <button
                     type="submit"
