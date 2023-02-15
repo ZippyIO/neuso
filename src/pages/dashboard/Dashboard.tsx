af994eddfe34313f2109d2a04232b30acbfc6829
Nick <ZippyIO@users.noreply.github.com>
2023-02-15 12:53:17 +1100

    ✨feat: change dashboard page layout

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,25 @@
+import EntryCards from '../../components/EntryCards';
 import EntryForm from '../../components/EntryForm';
 import MoodChart from '../../components/MoodChart';
 import Navbar from '../../components/Navbar';
+import Sidebar from '../../components/Sidebar';
 
 const Dashboard = () => (
-    <div className="flex flex-col">
-        <Navbar />
-        <MoodChart />
-        <EntryForm />
+    <div className="grid h-screen grid-cols-[1fr,50vw,2fr] grid-rows-[48px,1fr]">
+        <div className="col-span-3">
+            <Navbar />
+        </div>
+        <div className="bg-dark-800 row-start-2">
+            <Sidebar />
+        </div>
+        <div className="col-start-2 row-start-2">
+            <MoodChart />
+
+            <EntryCards />
+        </div>
+        <div className="bg-dark-800 col-start-3 row-start-2">
+            <EntryForm />
+        </div>
     </div>
 );
 
