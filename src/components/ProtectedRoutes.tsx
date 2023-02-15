c35e2f5a792209958f3f41336028e0cee53b16f9
Nick <ZippyIO@users.noreply.github.com>
2023-02-15 16:28:35 +1100

    ♻ refactor: removed unnecessary logic for redirect

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -4,10 +4,13 @@ import { useAuth } from '../context/AuthContext';
 const ProtectedRoutes = () => {
     const location = useLocation();
     const { user, loaded } = useAuth();
+    if (loaded) {
+        return <Outlet />;
+    }
 
     return (
+        // eslint-disable-next-line react/jsx-no-useless-fragment
         <>
-            {loaded && user !== null && <Outlet />}
             {!loaded && user === null && (
                 <Navigate to="/" state={{ from: location }} replace />
             )}
