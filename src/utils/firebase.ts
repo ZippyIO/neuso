21ce702e64672c89dabe59f7114112d5fabe7b1a
Nick <ZippyIO@users.noreply.github.com>
2023-02-15 12:02:52 +1100

    ✏ fix: fix MeasurementId typo

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
     storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FB_MSG_SENDER_ID,
     appId: import.meta.env.VITE_FB_APP_ID,
-    MessurementId: import.meta.env.VITE_FB_MESSUREMENT_ID,
+    MeasurementId: import.meta.env.VITE_FB_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
