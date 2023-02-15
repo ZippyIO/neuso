b1bbb85c870fd5eba693397277b7f6d6656f0189
Nick <ZippyIO@users.noreply.github.com>
2023-02-15 16:32:30 +1100

    ✨ feat: add browser location component + tsx file

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -2,6 +2,7 @@
 import { cleanup, render, RenderOptions } from '@testing-library/react';
 import { afterEach } from 'vitest';
 import fetch from 'node-fetch';
+import { useLocation } from 'react-router-dom';
 // @ts-ignore
 global.fetch = fetch;
 
@@ -9,12 +10,18 @@ afterEach(() => {
     cleanup();
 });
 
-const customRender = (ui: JSX.Element, options?: RenderOptions) =>
+const customRender = (ui: JSX.Element, options?: RenderOptions) => {
     render(ui, {
         // wrap provider(s) here if needed
         wrapper: ({ children }) => children,
         ...options,
     });
+};
+
+export const LocationDisplay = () => {
+    const location = useLocation();
+    return <div data-testid="location-display">{location.pathname}</div>;
+};
 
 export * from '@testing-library/react';
 export { default as userEvent } from '@testing-library/user-event';
