c98b76f49135e3a1777138ff921cd9952b7f5364
Nick <ZippyIO@users.noreply.github.com>
2023-02-15 12:42:51 +1100

    💄 feat: changed navbar colours

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,10 @@ import { NavLink } from 'react-router-dom';
 import Dropdown from './Dropdown';
 
 const Navbar = () => (
-    <div className="flex items-center justify-between bg-blue-600 px-4 py-2">
-        <h1 className="text-2xl font-bold text-zinc-100">Neuso</h1>
+    <div className="flex h-12 items-center justify-between bg-green-600 px-4 py-2">
+        <h1 className="font-montserrat text-3xl font-bold text-white">Neuso</h1>
         <div className="flex items-center gap-2">
-            <ul className="flex gap-2 text-zinc-100">
+            <ul className="flex gap-2 text-white">
                 <li className="text-inherit">
                     <NavLink to="/dashboard">Dashboard</NavLink>
                 </li>
