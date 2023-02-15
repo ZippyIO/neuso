211bac858f05c73518461065705aa1d2b2555759
Nick <ZippyIO@users.noreply.github.com>
2023-02-15 13:04:12 +1100

    💄 feat: change select colours

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -20,19 +20,23 @@ const Select = ({ values, selectedValues, handleValueChange }: SelectProps) => (
                 type="button"
                 className={clsx(
                     'inline-flex select-none items-center rounded-md px-4 py-2 text-sm font-medium',
-                    'bg-white text-gray-700 hover:bg-gray-50 dark:bg-zinc-900/40  dark:hover:bg-zinc-900/70', // dark:text-gray-100
+                    'dark:bg-dark-900/60 dark:hover:bg-dark-900 bg-white  hover:bg-gray-50',
                     'hover:bg-gray-50',
                     'focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75',
                     // Register all radix states
                     'group',
-                    'radix-state-open:bg-gray-50 dark:radix-state-open:bg-zinc-900',
+                    'radix-state-open:bg-gray-50 dark:radix-state-open:bg-dark-900',
                     'radix-state-on:bg-gray-50 dark:radix-state-on:bg-zinc-900',
                     'radix-state-instant-open:bg-gray-50 radix-state-delayed-open:bg-gray-50',
                 )}
             >
-                <SelectPrimitive.Value>Select Tags</SelectPrimitive.Value>
+                <SelectPrimitive.Value asChild>
+                    <span className="font-semibold text-zinc-400">
+                        Select Tags
+                    </span>
+                </SelectPrimitive.Value>
                 <SelectPrimitive.Icon>
-                    <ChevronDownIcon />
+                    <ChevronDownIcon color="#a1a1aa" />
                 </SelectPrimitive.Icon>
             </button>
         </SelectPrimitive.Trigger>
@@ -40,7 +44,7 @@ const Select = ({ values, selectedValues, handleValueChange }: SelectProps) => (
             <SelectPrimitive.ScrollUpButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
                 <ChevronUpIcon />
             </SelectPrimitive.ScrollUpButton>
-            <SelectPrimitive.Viewport className="rounded-lg border-4 border-zinc-600/80 bg-white p-2 shadow-lg shadow-black/20 dark:bg-zinc-700">
+            <SelectPrimitive.Viewport className="dark:bg-dark-400 border-dark-600 rounded-lg border-4 bg-white p-2 shadow-lg shadow-black/20">
                 <SelectPrimitive.Group>
                     {values.map((tag) => {
                         if (
@@ -56,7 +60,7 @@ const Select = ({ values, selectedValues, handleValueChange }: SelectProps) => (
                                     key={tag}
                                     value={tag.toLowerCase()}
                                     className={clsx(
-                                        'relative flex items-center gap-1.5 rounded-md px-4 py-2 text-sm font-medium',
+                                        'relative flex items-center gap-1.5 rounded-md px-4 py-2 text-sm font-medium text-gray-400',
                                         'radix-disabled:text-blue-700',
                                         'select-none focus:outline-none',
                                     )}
@@ -74,14 +78,14 @@ const Select = ({ values, selectedValues, handleValueChange }: SelectProps) => (
                                 key={tag}
                                 value={tag.toLowerCase()}
                                 className={clsx(
-                                    'relative flex items-center gap-1.5 rounded-md px-4 py-2 text-sm font-medium text-gray-700 focus:bg-gray-100 dark:text-gray-300 dark:focus:bg-gray-900/30',
+                                    'dark:focus:bg-dark-600 relative flex items-center gap-1.5 rounded-md px-4 py-2 text-sm font-medium focus:bg-gray-100',
                                     'radix-disabled:opacity-50',
                                     'select-none focus:outline-none',
                                 )}
                             >
-                                <PlusCircledIcon />
-                                <SelectPrimitive.ItemText>
-                                    {tag}
+                                <PlusCircledIcon color="rgb(209, 213, 219)" />
+                                <SelectPrimitive.ItemText asChild>
+                                    <span className="text-gray-300">{tag}</span>
                                 </SelectPrimitive.ItemText>
                             </SelectPrimitive.Item>
                         );
