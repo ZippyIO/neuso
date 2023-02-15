21fd84f09b8025362d311b6dd868bbd41eb18c23
Nick <ZippyIO@users.noreply.github.com>
2023-02-15 16:31:19 +1100

    ✨ feat: add redirect if auth logic

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
 
-const Home = () => (
-    <div className="flex h-screen w-screen items-center justify-center">
-        <div className="bg-dark-700 flex h-48 flex-col items-center justify-center gap-2 rounded-md px-8 shadow-lg">
-            <h1 className="font-montserrat text-2xl font-bold text-gray-200">
-                Nueso - Mood & Life Tracker
-            </h1>
-            <div className="flex gap-2">
-                <Link
-                    to="/login"
-                    className="h-8 w-36 rounded-md bg-green-600 py-1 px-8 text-center font-medium text-gray-100 transition-colors hover:bg-green-700 active:bg-green-800"
-                >
-                    Log in
-                </Link>
-                <Link
-                    to="/signup"
-                    className="h-8 w-36 rounded-md bg-green-600 py-1 px-8 text-center font-medium text-gray-100 transition-colors hover:bg-green-700 active:bg-green-800"
-                >
-                    Sign up
-                </Link>
+const Home = () => {
+    const location = useLocation();
+    const { loaded } = useAuth();
+
+    if (loaded) {
+        return <Navigate to="/dashboard" state={{ from: location }} replace />;
+    }
+
+    return (
+        <div
+            data-testid="page-home"
+            className="flex h-screen w-screen items-center justify-center"
+        >
+            <div className="bg-dark-700 flex h-48 flex-col items-center justify-center gap-2 rounded-md px-8 shadow-lg">
+                <h1 className="font-montserrat text-2xl font-bold text-gray-200">
+                    Nueso - Mood & Life Tracker
+                </h1>
+                <div className="flex gap-2">
+                    <Link
+                        to="/login"
+                        className="h-8 w-36 rounded-md bg-green-600 py-1 px-8 text-center font-medium text-gray-100 transition-colors hover:bg-green-700 active:bg-green-800"
+                    >
+                        Log in
+                    </Link>
+                    <Link
+                        to="/signup"
+                        className="h-8 w-36 rounded-md bg-green-600 py-1 px-8 text-center font-medium text-gray-100 transition-colors hover:bg-green-700 active:bg-green-800"
+                    >
+                        Sign up
+                    </Link>
+                </div>
             </div>
         </div>
-    </div>
-);
-
+    );
+};
 export default Home;
