bd39205a7b588a21209c98c19b35ab66985b927b
Nick <ZippyIO@users.noreply.github.com>
2023-02-15 11:56:46 +1100

    ✨ feat: add dark colour theme to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,22 @@ module.exports = {
             opensans: ['Open Sans', 'sans-serif'],
             montserrat: ['Montserrat', 'sans-serif'],
         },
-        extend: {},
+        extend: {
+            colors: {
+                dark: {
+                    50: '#2A2A2F',
+                    100: '#29292E',
+                    200: '#26262B',
+                    300: '#242429',
+                    400: '#222226',
+                    500: '#1F1F23',
+                    600: '#1D1D20',
+                    700: '#1A1A1E',
+                    800: '#18181B',
+                    900: '#161618',
+                },
+            },
+        },
     },
     plugins: [require('tailwindcss-radix')()],
 };
